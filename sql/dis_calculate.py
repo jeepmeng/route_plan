0c28a5e8389c19b6a3b30cfca76b56423c696835
jeepmeng <fucongliu0726@gmail.com>
2023-10-18 23:28:34 +0800

    1018mini

---
+++ sql/dis_calculate.py
@@ -0,0 +1,20 @@
+from math import asin, sqrt, cos, pi
+
+
+# point_a,point_b是经纬度,格式为[lng,lat]
+def st_distance(point_a, point_b):
+    # 弧度
+    radian = pi / 180.0
+    # 地球半径
+    radius = 6378137
+
+    f = point_a[1] * radian
+    h = point_b[1] * radian
+    k = 2 * radius
+    d = point_b[0] * radian - point_a[0] * radian
+    e = (1 - cos(h - f) + (1 - cos(d)) * cos(f) * cos(h)) / 2
+    return round(k * asin(sqrt(e)),1)
+
+if __name__ == '__main__':
+
+    print(st_distance([125.338530, 43.947330], [125.328812, 43.923599]))
