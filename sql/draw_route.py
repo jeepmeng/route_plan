11ca77311b0452961c4d40b8d8618ab6ec83716d
jeepmeng <fucongliu0726@gmail.com>
2023-10-24 18:02:06 +0800

    2023-10-24

--- sql/draw_route.py
+++ sql/draw_route.py
@@ -20,18 +20,23 @@ def best_map_speed(start,end,*args,strategy=0):
                   'destination': end,
                   'waypoints': args,
                   'strategy': strategy,
+        'show_fields':'cost,tmcs',
                   }  # 地址参数
-    url = 'https://restapi.amap.com/v3/direction/driving?'   # 高德地图地理编码API服务地址
+    # url = 'https://restapi.amap.com/v3/direction/driving?'   # 高德地图地理编码API服务地址
+    url = 'https://restapi.amap.com/v5/direction/driving?'  # 高德地图地理编码API服务地址
     result = requests.get(url, parameters)  # GET方式请求
     result = result.json()
+    # lon_lat = result["route"]["paths"][0]["distance"]  # 获取返回参数geocodes中的location，即经纬度
     lon_lat = result["route"]["paths"][0]["distance"]  # 获取返回参数geocodes中的location，即经纬度
-    output_path = result["route"]["paths"][0]
-    time = 0
-    for i in result["route"]["paths"][0]["steps"]:
-        time += int(i['duration'])
+    output_path = result["route"]
+    print(result["route"])
+    # time = 0
+    # for i in result["route"]["paths"][0]["steps"]:
+    #     time += int(i['duration'])
     # print(result)
     # print(lon_lat)
-    return lon_lat, time, output_path
+    # return lon_lat, time, output_path
+    return lon_lat, output_path
 
 path1 =[
     '新隆家园公交站',
@@ -126,29 +131,33 @@ path3 =[
 
 
 path3_real =[
-    '华大城一期公交站（Z110路）',
-'华大天朗国际公交站',
-'宽城区宽府路公交站',
-'长春市宽城区华侨城公交站',
-'长春市宽城区开关宿舍公交站',
-'长春市宽城区阿波罗网咖',
-'长春市宽城区密山路公交站',
-'长春市宽城区长欣家园公交站',
-'长春市宽城区聚旺角超市',
-'长春市宽城区天光路公交站',
-'长春市宽城区培智学校南门',
-'长春市第七十二中学（北校区）',
-'长春市第七十二中小学部',
-'长盛小学',
-'长新小学',
-'长春市宽城区实验小学'
+'华大城一期公交站（Z110路）',#125.333856,43.956194
+'华大天朗国际公交站（Z110路）',#125.344367,43.950086
+'宽城区宽府路公交站（G8路，Z110路，123路）',#125.330062,43.942557
+'宽城区庆丰路公交站（G8路，Z110路，123路）',#125.318977,43.874111
+'长春市宽城区华侨城公交站（G8路，G21路，Z110路，123路）',#125.340908,43.932803
+'长春市宽城区开关宿舍公交站（G11路）',#125.337159,43.927224
+'长春市宽城区劳模会馆公交站（G10路，G11路，G21路）',#125.349006,43.930739
+'长春市宽城区团山街公交站（G10路，G11路，G21路）',#125.355312,43.922259
+'长春市宽城区密山路公交站（G10路）',#125.322792,43.878319
+'长春市宽城区福山路公交站（G10路）',#125.360290,43.931243
+'长春市宽城区金美程家园东区（西2门对面停靠）',#125.349003,43.921807
+'长春市宽城区钻石村公交站（274路，Z274路）',#125.346145,43.921428
+'长春市宽城区天光路公交站（G8路，G11路，Z110路）',#125.332757,43.920230
+'长春市宽城区亚泰大街公交站（274路，Z274路）',#125.345041,43.899128
+'长春市宽城区培智学校南门',#125.331496,43.920632
+'长春市第七十二中学（北校区）',#125.328791,43.923203
+'长春市第七十二中小学部',#125.330128,43.924254
+'长盛小学',#125.319229,43.926578
+# '长新小学',#125.325018,43.929893
+# '长春市宽城区实验小学'#125.325453,43.927389
 
 
 ]
 
 
 
-path_1 = path1_real
+path_1 = path3_real
 
 
 temp_list = []
@@ -156,7 +165,11 @@ temp_wey_points = ''
 temp_wey_points_len = len(path_1)
 
 start = get_loc_2(path_1[0])
+# print(path_1[0])
+# print(start)
 end = get_loc_2(path_1[-1])
+# print(end)
+# print(path_1[-1])
 
 
 
@@ -165,7 +178,7 @@ end = get_loc_2(path_1[-1])
 wey_points = copy.deepcopy(path_1)
 wey_points.pop(0)
 wey_points.pop(-1)
-print(wey_points)
+print('wey_points------',wey_points)
 
 
 for k,i in enumerate(wey_points):
@@ -185,6 +198,7 @@ output = best_map_speed(start,end,temp_wey_points)
 
 
 if __name__ == '__main__':
+    pass
     lon_lat, time, output_path = best_map_speed(start, end, temp_wey_points)
     print('lon_lat-----------------',lon_lat)
     print('time-----------------', time)
@@ -193,7 +207,7 @@ if __name__ == '__main__':
 
     hh = json.dumps(output_path, indent=3, ensure_ascii=False)
     # save_name = str(path1_real)+'.json'
-    with open('path1_real.json', 'w') as f:
+    with open('path3_real.json', 'w') as f:
         f.write(hh)
         # f.write('\ntime={}'.format(time))
         # f.write('\nlength={}'.format(time))
